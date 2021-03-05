03ce409d95540e6c309af04296e2e60a9ecae2b9
sharonrosa <srwijnberg@gmail.com>
2021-03-05 15:36:02 +0100

    First Commit

---
+++ src/main/java/nl/wijnberg/menscreate/MensCreateApplication.java
@@ -0,0 +1,13 @@
+package nl.wijnberg.menscreate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MensCreateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MensCreateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/wijnberg/menscreate/MensCreateApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.wijnberg.menscreate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MensCreateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
